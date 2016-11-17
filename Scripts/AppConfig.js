82330718c41b67738768a85f969da2ea6b31cc72
juanpablogd <juanpablogarzond@gmail.com>
2016-11-17 17:54:49 -0500

    Permisos App

--- Scripts/AppConfig.js
+++ Scripts/AppConfig.js
@@ -7,7 +7,7 @@ var AppConfig={
 		//ADMIN           (18)			// tipo 'C'
 		//CONSULTA        (2)				// tipo 'C'
 		//77,89=Planeación -- 91=Administrativo -- 110=Gobierno\Rendición 
- 	id_perfil:[2,18,77,89,91,110], 					// tipo 'C'
+ 	id_perfil:[2,18,77,89,91,110,19,20,21,77,101,105,118,119,120,121,122,123,124,125,126,127,128,130,131,132,133,134,135,136,137,138,139,140,141,142], 					// tipo 'C'
  	
 	 	//ADMIN           (24,69)			// tipo 'A'
 	 	//EDICIÓN Y CARGA (79,87,96)		// tipo 'A'

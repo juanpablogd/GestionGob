c63e1672b9a3d8c29331bac51f30322139ced6fc
Juan Pablo GD <juanpablogarzond@gmail.com>
2017-06-29 17:30:41 -0500

    Opciones Listado
    Detalle
    Adicionar visita
    Editar
    Eliminar

--- Scripts/Gestiones/Gestiones.js
+++ Scripts/Gestiones/Gestiones.js
@@ -1,90 +1,52 @@
-$(document).ready(function() {	//https://datatables.net/examples/ajax/null_data_source.html     
-//https://stackoverflow.com/questions/31327933/how-add-more-then-one-button-in-each-row-in-jquery-datatables-and-how-to-apply-e
-//https://www.google.com.co/search?q=datatable+enable+function&oq=datatable+enable+function&aqs=chrome..69i57.10807j0j7&sourceid=chrome&ie=UTF-8#q=datatable+button+for+row
-//https://stackoverflow.com/questions/22471862/how-to-add-button-on-each-row-in-datatable 
+$(document).ready(function() {
 	var oTable;
+	var txtCol;
+	if(Func.GetTipo()=="C"){
+		txtCol = '<a href="#" class="btn_detalle" data-toggle="tooltip" title="Ver Detalle"><i class="fa fa-outdent" aria-hidden="true"></i></a>';
+	} else{
+		txtCol = '<a href="#" class="btn_detalle" data-toggle="tooltip" title="Ver Detalle"><i class="fa fa-outdent" aria-hidden="true"></i></a></br><a href="#" class="btn_add_visita" data-toggle="tooltip" title="Adicionar Visita"><i class="fa fa-plus-square" aria-hidden="true"></i></a></br><a href="#" class="btn_editar" data-toggle="tooltip" title="Editar Gestión"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></br><a href="#" class="btn_eliminar" data-toggle="tooltip" title="Eliminar Gestión"><i class="fa fa-trash" aria-hidden="true"></i></a>';
+	}
+	var columnDefs = [ 
+		{
+          "targets": -1,
+          "data": null,
+          "defaultContent": txtCol
+        },
+        {
+          	"targets": [ 7 ],
+            "visible": false,
+            "searchable": false
+        }
+	];
 	var configTBL = {
-					"aLengthMenu": [[50, 10, 20, 100], [50, 10, 20, 100]],
-					"oLanguage": {
-								"sProcessing":     "Procesando...",
-								"sLengthMenu":     "Mostrar _MENU_ registros",
-								"sZeroRecords":    "No se encontraron resultados",
-								"sEmptyTable":     "Ningún dato disponible en esta tabla",
-								"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-								"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-								"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-								"sInfoPostFix":    "",
-								"sSearch":         "Buscar:",
-								"sUrl":            "",
-								"sInfoThousands":  ",",
-								"sLoadingRecords": "Cargando...",
-								"oPaginate": {
-									"sFirst":    "Primero",
-									"sLast":     "Último",
-									"sNext":     "Siguiente",
-									"sPrevious": "Anterior"
-								},
-								"oAria": {
-									"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-									"sSortDescending": ": Activar para ordenar la columna de manera descendente"
-								}
+		"aLengthMenu": [[50, 10, 20, 100], [50, 10, 20, 100]],
+		"oLanguage": {
+					"sProcessing":     "Procesando...",
+					"sLengthMenu":     "Mostrar _MENU_ registros",
+					"sZeroRecords":    "No se encontraron resultados",
+					"sEmptyTable":     "Ningún dato disponible en esta tabla",
+					"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+					"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+					"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+					"sInfoPostFix":    "",
+					"sSearch":         "Buscar:",
+					"sUrl":            "",
+					"sInfoThousands":  ",",
+					"sLoadingRecords": "Cargando...",
+					"oPaginate": {
+						"sFirst":    "Primero",
+						"sLast":     "Último",
+						"sNext":     "Siguiente",
+						"sPrevious": "Anterior"
 					},
-					"deferRender": true,
-					"aaSorting": [] 
-				};
-	var Eventos = function(){
-	  	$(".btn_add_visita").click(function(){		//console.log("Click");	console.log($(this).attr( 'val' ));
-	  		var fila = $(this).attr('f');
-	  		var NomG = $('#d'+fila).text(); //console.log(NomG);
-	  		Func.SetNomGestion(NomG);
-	  		Func.SetIdGestion($(this).attr( 'val' ));
-	  		setTimeout(function(){
-		    	window.location.href = 'AdicionarVisita.html';
-			}, 50);
-		});
-		$(".btn_editar").click(function(){		console.log($(this).attr( 'val' ));
-	  		var fila = $(this).attr('f');
-	  		var NomG = $('#d'+fila).text(); //console.log(NomG);
-	  		Func.SetNomGestion(NomG);
-	  		Func.SetIdGestion($(this).attr( 'val' ));
-	  		setTimeout(function(){
-	  			window.open(
-				  'Editar.html',
-				  '_blank' // <- This is what makes it open in a new window.
-				); 
-			}, 50);
-		});
-		$(".btn_eliminar").click(function(){	console.log("Click ELiminar");
-			var fila = $(this).attr('f');				//console.log(fila);	//console.log();
-			var id_gestion = $(this).attr('val' );		//console.log(id_gestion);	//console.log($.fn.dataTable.isDataTable( oTable ));
-			bootbox.confirm('<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color:red"></i> Seguro que desea <B>Eliminar</B> la gestión: '+$('#d'+fila).text(), function(result) {
-			  	if(result){
-		  			AppConfig.socketDataAdmin = io.connect(AppConfig.UrlSocketApp+'/DataAdmin');	 //console.log("Cliente:"+AppConfig.socketDataAdmin.io.engine.id);		
-				  	AppConfig.socketDataAdmin.emit('DeleteGestion', {io_id : AppConfig.socketDataAdmin.io.engine.id, id_gestion : id_gestion }, function(message){			
-				  		console.log(message);
-				  		if(message=="Ok"){	
-				  			oTable.row('#f'+fila).remove().draw( false );	console.log('Removido');
-				  		}
-					});
-			  	}
-			});
-		});
-		$(".btn_detalle").click(function(){	console.log("Click Detalle");
-	  		var fila = $(this).attr('f');
-	  		var NomG = $('#d'+fila).text(); //console.log(NomG);
-	  		Func.SetNomGestion(NomG);
-	  		Func.SetIdGestion($(this).attr( 'val' ));
-	  		setTimeout(function(){
-	  			window.open(
-				  'Detalle.html',
-				  '_blank' // <- This is what makes it open in a new window.
-				);
-		    	//window.location.href = 'Detalle.html';
-			}, 50);
-		});
+					"oAria": {
+						"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+						"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+					}
+		},
+		"columnDefs": columnDefs
 	};
-	var CargarGestiones = function(usrTipo,centroGestor) {
-		//OPTIMIZAR CARGA	http://jsfiddle.net/V2Kdz/
+	var CargarGestiones = function(usrTipo,centroGestor) {	//OPTIMIZAR CARGA	http://jsfiddle.net/V2Kdz/
 		$('#TBList tbody > tr').remove(); //console.log($('#TBList tr').length);	//console.log($('#TBody').length);
 		 $('#TBList tfoot th').each( function () {
 	        var title = $(this).text();
@@ -93,31 +55,18 @@ $(document).ready(function() {	//https://datatables.net/examples/ajax/null_data_
 		
 		AppConfig.socketDataAdmin = io.connect(AppConfig.UrlSocketApp+'/DataAdmin');
 	  	AppConfig.socketDataAdmin.emit('GetListadoGes',  {usrTipo : usrTipo, id_centrog : centroGestor}, function(message){				//console.log("message Mun DATA: " + message.length); //console.log("message Mun:" + message);
-			console.log(moment().format('h:mm:ss:SSSS')+" Listado Gestiones Ini");			//console.log("message:" + message);
-			var decrypted = FuncDecrypted(message);
-			if(Func.GetTipo()=="C"){
-				$("#TBList tr td,th").filter(':nth-child(9)').remove();
-				$("#TBList tr td,th").filter(':nth-child(9)').remove();
-				$("#TBList tr td,th").filter(':nth-child(9)').remove();				
-			}
+			console.log(moment().format('h:mm:ss:SSSS')+" Listado Gestiones Ini");			//console.log(message);
+			var decrypted = FuncDecrypted(message);		//console.log(decrypted);
 			$.each(decrypted, function () {
 				$.each(this, function (name1, value1) {		//console.log(value1);	//console.log(name1 + '=' + value1); 
 					$("#TBody").append('<tr id="f'+name1+'"></tr>');
 					$.each(value1, function (name, value) {	//console.log(name + '=' + value);
 						if(name=="avance"){
 							if(value=="0") $('#f'+name1).css('color', 'red'); 
-						} else {	
-							if(name!="id_gestion"){
-								if(name=="descripcion"){
-									$('#f'+name1).append('<td id="d'+name1+'">'+value+'</td>');
-								}else $('#f'+name1).append("<td>"+value+"</td>");
-							} else {
-								if(Func.GetTipo()=="C"){
-									$('#f'+name1).append('<td><a href="#" class="btn_detalle" val="'+value+'" f="'+name1+'"><i class="fa fa-outdent" aria-hidden="true"></i></a></td>');
-								} else{
-									$('#f'+name1).append('<td><a href="#" class="btn_detalle" val="'+value+'" f="'+name1+'"><i class="fa fa-outdent" aria-hidden="true"></i></a></td><td><div><h4><a href="#" class="btn_add_visita" val="'+value+'" f="'+name1+'"><i class="fa fa-plus-square" aria-hidden="true"></i></a></h4></div></td><td><a href="#" class="btn_editar" val="'+value+'" f="'+name1+'"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td><td><a href="#" class="btn_eliminar" val="'+value+'" f="'+name1+'"><i class="fa fa-trash" aria-hidden="true"></i></a></td>');								
-								}
-							}
+						} else {
+							if(name=="id_gestion"){
+								$('#f'+name1).append("<td>"+value+"</td><td></td>");
+							}else $('#f'+name1).append("<td>"+value+"</td>");
 						}
 			      	});
 				}); //console.log("Cargaaaaaa");
@@ -134,8 +83,50 @@ $(document).ready(function() {	//https://datatables.net/examples/ajax/null_data_
 		            }
 		        } );
 		    } );
+		    $('#TBList tbody').on( 'click', 'a', function () {	//console.log($(this).attr("class"));
+		        var data = oTable.row( $(this).parents('tr') ).data(); //console.log(data);
+		        var tipo = $(this).attr("class");
+		        if(tipo=="btn_detalle"){
+			  		Func.SetNomGestion(data[2]);
+			  		Func.SetIdGestion(data[7]);
+			  		setTimeout(function(){
+			  			window.open(
+						  'Detalle.html',
+						  '_blank' // <- This is what makes it open in a new window.
+						);
+					}, 50);
+		        }else if(tipo=="btn_add_visita"){
+			  		Func.SetNomGestion(data[2]);
+			  		Func.SetIdGestion(data[7]);
+			  		setTimeout(function(){
+				    	window.location.href = 'AdicionarVisita.html';
+					}, 50);
+		        }else if(tipo=="btn_editar"){
+			  		Func.SetNomGestion(data[2]);
+			  		Func.SetIdGestion(data[7]);
+			  		setTimeout(function(){
+			  			window.open(
+						  'Editar.html',
+						  '_blank' // <- This is what makes it open in a new window.
+						); 
+					}, 50);
+		        }else if(tipo=="btn_eliminar"){
+					var fila = data["DT_RowId"];				//console.log(fila);	console.log($(this)[0]);
+					bootbox.confirm('<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color:red"></i> Seguro que desea <B>Eliminar</B> la gestión: '+data[2], function(result) {
+					  	if(result){
+				  			AppConfig.socketDataAdmin = io.connect(AppConfig.UrlSocketApp+'/DataAdmin');	 //console.log("Cliente:"+AppConfig.socketDataAdmin.io.engine.id);		
+						  	AppConfig.socketDataAdmin.emit('DeleteGestion', {io_id : AppConfig.socketDataAdmin.io.engine.id, id_gestion : data[7] }, function(message){			
+						  		console.log(message);
+						  		if(message=="Ok"){	
+						  			oTable.row('#'+fila).remove().draw( false );	console.log('Removido');
+						  		}
+							});
+					  	}
+					});
+		        }	//console.log( data[0] +"'s salary is: "+ data[ 7 ] );
+		    } );	
 		    console.log(moment().format('h:mm:ss:SSSS')+" Mun Gestiones FIN");	//console.log($.fn.dataTable.isDataTable( '#TBList' ));
-		  	Eventos();
+		  	$('[data-toggle="tooltip"]').tooltip(); 
 		});
 	};
 	console.log("Carga Inicial de Gestiones!");
@@ -143,4 +134,4 @@ $(document).ready(function() {	//https://datatables.net/examples/ajax/null_data_
 	if(Func.GetTipo()=="C"){
 		$("#div_add").hide();
 	}
-}); 
+});
